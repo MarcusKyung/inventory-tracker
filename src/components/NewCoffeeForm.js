521070b4cfb7d39ffe5cb86c6ec6e6387a04668d
MarcusKyung <Kyungmj@gmail.com>
2023-06-30 10:44:55 -0700

    add new coffee and edit coffee form components

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { v4 } from 'uuid'; 
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewCoffeeForm(props){ 
+  function handleNewCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCoffeeCreation({
+      name: event.target.name.value, 
+      origin: event.target.origin.value, 
+      roast: event.target.roast.value, 
+      description: event.target.description.value, 
+      price: parseInt(event.target.price.value),
+      quantity: parseInt(event.target.quantity.value),
+      productImage: event.target.productImage.value,
+      id: v4()
+    });
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleNewCoffeeFormSubmission}
+        buttonText="Add Coffee" />
+    </React.Fragment>
+  );
+}
+
+NewCoffeeForm.propTypes = {
+  onNewCoffeeCreation: PropTypes.func 
+};
+
+
+export default NewCoffeeForm;
