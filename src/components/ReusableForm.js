15ad2b47f576d7e92642eccb28cdc92e2cb9fca9
MarcusKyung <Kyungmj@gmail.com>
2023-06-30 14:04:56 -0700

    add required to Reusable form component

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -5,16 +5,17 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input type='text' name='name' placeholder='Coffee Bean Name' />
-        <input type='text' name='origin' placeholder='Coffee origin' />
-        <input type='text' name='roast' placeholder='Coffee Roast' />
-        <input type='number' name='price' placeholder='Coffee price' />
-        <input type='text' name='description' placeholder='Coffee description'/>
-        <input type='number' name='quantity' placeholder='Coffee quantity'/>
-        <select name="productImage">
-          <option value="image1.jpg">Image 1</option>
-          <option value="image2.jpg">Image 2</option>
-          <option value="image3.jpg">Image 3</option>
+        <input type='text' name='name' placeholder='Coffee Bean Name' required/>
+        <input type='text' name='origin' placeholder='Coffee origin' required/>
+        <input type='text' name='roast' placeholder='Coffee Roast' required/>
+        <input type='number' name='price' placeholder='Coffee price' required/>
+        <input type='text' name='description' placeholder='Coffee description' required/>
+        <input type='number' name='quantity' placeholder='Coffee quantity' required/>
+        <select name="productImage" required>
+          <option value="Coffee1">Image 1</option>
+          <option value="Coffee2">Image 2</option>
+          <option value="Coffee3">Image 3</option>
+          <option value="Coffee4">Image 3</option>
         </select>
         <button type='submit'>{props.buttonText}</button>
       </form>
