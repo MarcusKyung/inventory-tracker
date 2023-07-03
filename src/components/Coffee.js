ae19f8a4a1715037ec640863ad1d6dbd70d6139d
MarcusKyung <Kyungmj@gmail.com>
2023-07-02 17:27:56 -0700

    update card styling

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -7,11 +7,11 @@ function Coffee(props){
       <div onClick = {() => props.whenCoffeeClicked(props.id)}> 
         <h3 style={{textAlign: "center" , marginTop: "10px"}}>{props.name} Coffee Beans</h3>
         <img src={props.productImage} style={{ display: 'block', margin: '0 auto' }}/>
-        <p><em>Country of Origin: {props.origin}</em></p>
-        <p>Roast Type: {props.roast}</p>
-        <p>Quantity: {props.quantity}</p>
-        <p>Description: {props.description}</p>
-        <p>Price: ${props.price}/bag</p>
+        <p><b>Country of Origin:</b> {props.origin}</p>
+        <p><b>Roast Type:</b> {props.roast}</p>
+        <p><b>Quantity:</b> {props.quantity}</p>
+        <p><b>Description:</b> {props.description}</p>
+        <p><b>Price:</b> ${props.price}/bag</p>
       </div>
     </React.Fragment>
   );
--- src/components/CoffeeDetails.js
+++ src/components/CoffeeDetails.js
@@ -13,13 +13,14 @@ function CoffeeDetail(props){
       <Card style={{ width: '35rem', outline: '2px solid', background: 'lightGrey' }}>      
         <Card.Img className="rounded" variant="top" src={coffee.productImage} alt="coffee image" style = {{ marginTop: 10, outline: "1px solid"}}/>
         <Card.Body>
-          <Card.Title>Coffee Detail</Card.Title>
-          <Card.Subtitle>{coffee.name} Beans - from: {coffee.origin}</Card.Subtitle>
+          <Card.Title>Coffee Details:</Card.Title>
+          <Card.Subtitle>{coffee.name} Beans - <em>Country of Origin: {coffee.origin}</em></Card.Subtitle>
+          <br />
           <ul>
-            <li>Roast: {coffee.roast}</li>
-            <li>Description: {coffee.description}</li>
-            <li>Price: {coffee.price}</li>
-            <li>Quantity in Stock: {coffee.quantity > 0 ? coffee.quantity : "Out of Stock"}</li>
+            <li><b>Roast:</b> {coffee.roast}</li>
+            <li><b>Description:</b> {coffee.description}</li>
+            <li><b>Price:</b> {coffee.price}</li>
+            <li><b>Quantity in Stock:</b> {coffee.quantity > 0 ? coffee.quantity : "Out of Stock"}</li>
           </ul>
           <ButtonGroup>
             <Button variant="success" onClick={ props.onClickingEdit }>Update Coffee</Button>
